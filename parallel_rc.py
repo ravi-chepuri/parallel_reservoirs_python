e2244b047e0ec1c46f3b1cb26c150bf072651101
Ravi Chepuri <rchepuri99@gmail.com>
2024-07-23 13:48:03 -0400

    Revised basic routines for reservoir computing, and got basic single RC forecasting of Lorenz system working with the new routines

--- parallel_rc.py
+++ parallel_rc.py
@@ -8,10 +8,12 @@ To run, first install mpi4py in your conda environment (`conda install -c conda-
 from mpi4py import MPI
 import numpy as np
 
+import rc
+
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()  # process number
 size = comm.Get_size()  # total number of processes
-
 
 # input data specifications
 Q = 64  # number of spatial 1D grid points
@@ -22,21 +24,7 @@ T = 100  # number of time steps of training data
 q = 2  # number of contiguous grid points per reservoir
 l = 1  # number of spatial points in the contiguous buffer regions
 
+assert Q % q == 0, "Number of grid points is not a multiple of group size"
+assert size >= Q//q, "Number of processes is less than number of grid points"
+assert l <= q, "Size of buffer region is too large"
 
-def train_vanilla_RC(resparams, data):
-    """TODO"""
-    N = resparams['N']
-    radius = resparams['radius']
-    degree = resparams['degree']
-    num_inputs = resparams['num_inputs']
-    sigma = resparams['sigma']
-    train_length = resparams['train_length']
-
-    A = generate_reservoir.erdos_renyi_reservoir(N, degree, radius)
-    W_in = generate_reservoir.generate_W_in(num_inputs, N, sigma)
-
-    res_states = rc.reservoir_layer(A, W_in, data, resparams)
-
-    W_out = rc.fit_output_weights(resparams, res_states, data[:, :train_length])
-
-    return W_out, A, W_in, res_states
--- rc.py
+++ rc.py
@@ -1,39 +1,98 @@
 import numpy as np
+from numpy.random import default_rng
+from scipy import sparse
+from scipy.sparse import linalg
+from sklearn.linear_model import Ridge
+from dataclasses import dataclass
 
-def reservoir_layer(A, W_in, input, resparams):
+@dataclass
+class Hyperparameters:
+    # RC
+    num_inputs : int = 3
+    N : int = 50
+    degree : float = 10.
+    radius : float = 0.9
+    leakage : float = 1.0
+    bias : float = 0.5
+    sigma : float = 1.
+    discard_transient_length : int = 1000
+    beta : float = 1e-8
+    activation_func : '...' = np.tanh
+    # data
+    dt : float = 0.06
+    int_dt : float = 0.001  # integration time step
+    train_length : int = 10000
+    prediction_steps : int = 10000
+
+
+def _rescale_matrix(A, spectral_radius):
+    if not isinstance(A, sparse.csr_matrix):
+        A = sparse.csr_matrix(A)
+    eigenvalues, _ = linalg.eigs(A)
+    max_eigenvalue = max(abs(eigenvalues))
+    A_scaled = A / max_eigenvalue * spectral_radius
+    return(A_scaled)
+
+
+def _make_reservoir(h: Hyperparameters):
+    rng = default_rng()
+    nonzero_mat = rng.random((h.N, h.N)) < h.degree / h.N
+    random_weights_mat = -1 + 2 * rng.random((h.N, h.N)) # uniform distribution on [-1, 1] at each matrix element
+    A = nonzero_mat * random_weights_mat
+    A[A == -0.0] = 0.0
+    sA = sparse.csr_matrix(A)
+    try:
+        sA_rescaled = _rescale_matrix(sA, h.radius)
+        return sA_rescaled
+    except linalg.eigen.arpack.ArpackNoConvergence:
+        return _make_reservoir(h)
+        
+
+def _make_input_matrix(h: Hyperparameters):
+    rng = default_rng()
+    W_in = h.sigma * (-1 + 2 * rng.random((h.N, h.num_inputs)))
+    return W_in
+
+
+def _run_open_loop(A, W_in, input, h: Hyperparameters):
     """TODO"""
-    N = resparams['N']
-    train_length = resparams['train_length']
-    g = resparams['nonlinear_func']
-    bias = resparams['bias']
-
-    # Open loop
-    res_states = np.zeros((N, train_length))
-    for i in range(train_length - 1):
-        res_states[:, i+1] = g(A @ res_states[:, i] + W_in @ input[:, i] + bias)
-
+    res_states = np.zeros((h.N, h.train_length))
+    for t in range(h.train_length - 1):
+        res_states[:, t+1] = (1 - h.leakage) * res_states[:, t] \
+                            + h.leakage * h.activation_func(A @ res_states[:, t] \
+                                                            + W_in @ input[:, t+1] \
+                                                            + h.bias)
     return res_states
 
 
-def fit_output_weights(resparams, res_states, data, discard=0):
+def _fit_output_weights(res_states, data, h: Hyperparameters):
     """TODO"""
-    beta = resparams['beta']
-    N = resparams['N']
-    
-    # Tikhonov ridge regression using normal equations
-    W_out = data @ res_states.T @ np.linalg.pinv(res_states @ res_states.T + beta * np.identity(N))
+    assert res_states.shape[1] == h.train_length
+    res_states_for_fit = res_states[:, h.discard_transient_length:-1]
+    targets_for_fit = data[:, h.discard_transient_length+1:h.train_length]
+    clf = Ridge(alpha=h.beta, fit_intercept=False, solver='cholesky')
+    clf.fit(res_states_for_fit.T, targets_for_fit.T)
+    W_out = clf.coef_
     return W_out
 
-def predict(W_out, A, W_in, training_res_states, time_steps, resparams):
-    """Closed loop prediction"""
-    g = resparams['nonlinear_func']
-    bias = resparams['bias']
-    final_res_state = training_res_states[:, -1]
 
-    predictions = np.zeros((W_out.shape[0], time_steps))
-    rt = final_res_state
-    for i in range(time_steps):
-        predictions[:, i] = W_out @ rt
-        rt = g(A @ rt + W_in @ predictions[:, i] + bias)
-    
+def train_RC(data, h: Hyperparameters):
+    """TODO"""
+    A = _make_reservoir(h)
+    W_in = _make_input_matrix(h)
+    res_states = _run_open_loop(A, W_in, data, h)
+    W_out = _fit_output_weights(res_states, data, h)
+    return W_out, A, W_in, res_states
+
+
+def predict(W_out, A, W_in, training_res_states, h: Hyperparameters):
+    """Closed loop prediction"""
+    predictions = np.zeros((h.num_inputs, h.prediction_steps))
+    rt = training_res_states[:, -1]
+    for t in range(h.prediction_steps):
+        predictions[:, t] = W_out @ rt
+        rt = (1 - h.leakage) * rt \
+             + h.leakage * h.activation_func(A @ rt \
+                                             + W_in @ predictions[:, t] \
+                                             + h.bias)
     return predictions
