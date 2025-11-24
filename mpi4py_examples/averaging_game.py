a80dde3e69e85fa63605fa5cf1e8bd86547369eb
Ravi Chepuri <rchepuri99@gmail.com>
2025-11-24 14:20:37 -0500

    Switched averaging game to use non-blocking communication
    Now uses the more principled Irecv + Isend + Waitall pattern, which prevents deadlocks. This pattern is better for when multiple processes communicate simultaneously

--- mpi4py_examples/averaging_game.py
+++ mpi4py_examples/averaging_game.py
@@ -56,16 +56,43 @@ sim_length = 4
 my_log = np.empty(sim_length+1)
 my_log[0] = my_number
 print(f'Agent {my_agent_id}, time 0: My number is {my_number}')
+
+# OLD: blocking sends and received leads to potential deadlocks
+# for t in range(1, sim_length+1):
+#     for downstream_neighbor in my_downstream_neighbors:
+#         comm.Send(my_number, dest=downstream_neighbor, tag=t)
+#         print(f'Agent {my_agent_id}, time {t}: Sent my number {my_number} to agent {downstream_neighbor}')
+#     for i, upstream_neighbor in enumerate(my_upstream_neighbors):
+#         comm.Recv(my_incoming_buffers[i], source=upstream_neighbor, tag=t)
+#         print(f'Agent {my_agent_id}, time {t}: Received number {my_number} from agent {upstream_neighbor}')
+#     received = np.array(my_incoming_buffers)
+#     my_number[...] = np.mean(received)
+#     my_log[t] = my_number
+#     print(f'Agent {my_agent_id}, time {t}: My number is {my_number}')
+
+
 for t in range(1, sim_length+1):
-    for downstream_neighbor in my_downstream_neighbors:
-        comm.Send(my_number, dest=downstream_neighbor, tag=t)
-        print(f'Agent {my_agent_id}, time {t}: Sent my number {my_number} to agent {downstream_neighbor}')
+    # 1) Post all receives *first*
+    recv_reqs = []
     for i, upstream_neighbor in enumerate(my_upstream_neighbors):
-        comm.Recv(my_incoming_buffers[i], source=upstream_neighbor, tag=t)
-        print(f'Agent {my_agent_id}, time {t}: Received number {my_number} from agent {upstream_neighbor}')
-    received = np.array(my_incoming_buffers)
+        req = comm.Irecv(my_incoming_buffers[i], source=upstream_neighbor, tag=t)
+        recv_reqs.append(req)
+        print(f'Agent {my_agent_id}, time {t}: Posted Irecv from agent {upstream_neighbor}', flush=True)
+
+    # 2) Then post all sends
+    send_reqs = []
+    for downstream_neighbor in my_downstream_neighbors:
+        req = comm.Isend(my_number, dest=downstream_neighbor, tag=t)
+        send_reqs.append(req)
+        print(f'Agent {my_agent_id}, time {t}: Posted Isend of {my_number} to agent {downstream_neighbor}', flush=True)
+
+    # 3) Wait for all communication to complete before updating my_number
+    MPI.Request.Waitall(recv_reqs + send_reqs)
+
+    # 4) Compute new value from what we actually received
+    received = np.array([buf.item() for buf in my_incoming_buffers])
     my_number[...] = np.mean(received)
     my_log[t] = my_number
-    print(f'Agent {my_agent_id}, time {t}: My number is {my_number}')
+    print(f'Agent {my_agent_id}, time {t}: My number is {my_number}', flush=True)
 
 np.savetxt(f'{my_agent_id}.csv', my_log)  # logs match the result of working the game out by hand!
